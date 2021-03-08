40696793cfa0caa95bbc82946bb0bca859459f6e
benbd5 <belzbenjaminp@gmail.com>
2021-03-08 09:06:27 +0100

    ajout departements en navbar+ajustements css

--- serveur.js
+++ serveur.js
@@ -14,7 +14,6 @@ const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 
 // Swagger documentation
-
 const swaggerOptions = {
   swaggerDefinition: {
     info: {
